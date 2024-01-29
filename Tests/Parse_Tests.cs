fb94cbf31c6e4ebc253401b010a044f63f064909
NickStrupat <nick.strupat@gmail.com>
2024-01-28 23:31:15 -0500

    more tests

--- Tests/Parse_Tests.cs
+++ Tests/Parse_Tests.cs
@@ -51,4 +51,106 @@ public class Parse_Tests
             Assert.Equal(a ? 2 : 0, length);
         }
     }
+    
+    [Fact]
+    public void Range()
+    {
+        var range = Parser.Range(('a', 'z'));
+        for (var i = Char.MinValue; i != Char.MaxValue; i++)
+        {
+            if (Char.IsSurrogate(i))
+                continue;
+            var inRange = i is >= 'a' and <= 'z';
+            Assert.Equal(
+                inRange,
+                range.TryMatch([i], out var length)
+            );
+            Assert.Equal(inRange ? 1 : 0, length);
+        }
+    }
+    
+    [Fact]
+    public void Not()
+    {
+        var not = Parser.Not(Parser.Literal('a'));
+        for (var i = Char.MinValue; i != Char.MaxValue; i++)
+        {
+            if (Char.IsSurrogate(i))
+                continue;
+            var notA = i != 'a';
+            Assert.Equal(
+                notA,
+                not.TryMatch([i], out var length)
+            );
+            Assert.Equal(notA ? 1 : 0, length);
+        }
+    }
+    
+    [Fact]
+    public void Digit()
+    {
+        var digit = Parser.Digit();
+        for (var i = Char.MinValue; i != Char.MaxValue; i++)
+        {
+            if (Char.IsSurrogate(i))
+                continue;
+            var isDigit = Char.IsDigit(i);
+            Assert.Equal(
+                isDigit,
+                digit.TryMatch([i], out var length)
+            );
+            Assert.Equal(isDigit ? 1 : 0, length);
+        }
+    }
+    
+    [Fact]
+    public void Whitespace()
+    {
+        var whitespace = Parser.Whitespace();
+        for (var i = Char.MinValue; i != Char.MaxValue; i++)
+        {
+            if (Char.IsSurrogate(i))
+                continue;
+            var isWhitespace = Char.IsWhiteSpace(i);
+            Assert.Equal(
+                isWhitespace,
+                whitespace.TryMatch([i], out var length)
+            );
+            Assert.Equal(isWhitespace ? 1 : 0, length);
+        }
+    }
+    
+    [Fact]
+    public void Word()
+    {
+        var word = Parser.Word();
+        for (var i = Char.MinValue; i != Char.MaxValue; i++)
+        {
+            if (Char.IsSurrogate(i))
+                continue;
+            var isWord = Char.IsLetterOrDigit(i) | i == '_';
+            Assert.Equal(
+                isWord,
+                word.TryMatch([i], out var length)
+            );
+            Assert.Equal(isWord ? 1 : 0, length);
+        }
+    }
+    
+    [Fact]
+    public void AnyOneOf()
+    {
+        var anyOneOf = Parser.Literal('a').Or(Parser.Literal('b')).Or(Parser.Literal('c'));
+        for (var i = Char.MinValue; i != Char.MaxValue; i++)
+        {
+            if (Char.IsSurrogate(i))
+                continue;
+            var isAnyOneOf = i is 'a' or 'b' or 'c';
+            Assert.Equal(
+                isAnyOneOf,
+                anyOneOf.TryMatch([i], out var length)
+            );
+            Assert.Equal(isAnyOneOf ? 1 : 0, length);
+        }
+    }
 }
